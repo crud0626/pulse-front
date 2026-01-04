ad0666927996c9bc144897d2f6d03286f581c75a
crud0626 <sisofiy6048@gmail.com>
2026-01-04 22:05:12 +0900

    chore: axios, vite-plugin-svgr 패키지 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { reactRouter } from '@react-router/dev/vite';
+import svgr from 'vite-plugin-svgr';
 import autoprefixer from 'autoprefixer';
 import pandacss from '@pandacss/dev/postcss';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
- 
+
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [pandacss, autoprefixer]
-    }
+      plugins: [pandacss, autoprefixer],
+    },
   },
-  plugins: [reactRouter(), tsconfigPaths()]
-})
+  plugins: [reactRouter(), tsconfigPaths(), svgr()],
+});
