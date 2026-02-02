781dc751478475973e247d3d3d85b466578e9839
crud0626 <sisofiy6048@gmail.com>
2026-02-02 22:28:29 +0900

    feature: BottomSheet 컴포넌트 추가

--- app/root.tsx
+++ app/root.tsx
@@ -38,6 +38,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       >
         <div
           className={css({
+            position: 'relative',
             margin: 'auto',
             maxWidth: '360px',
             height: '100%',
